a1ec05a681dc59b661e26bc6f9d655b437a97e1a
Rohit Sinha <rsinha@berkeley.edu>
2015-05-29 10:00:54 -0700

    parsing init and next state assignments

--- src/main/scala/UclidMain.scala
+++ src/main/scala/UclidMain.scala
@@ -7,6 +7,7 @@ object UclidMain {
   def main(args: Array[String]) : Unit = {
     println("Input File: " + args(0))
     val input = scala.io.Source.fromFile(args(0)).mkString
-    println("result: " + UclidParser.parseModule(input))
+    val module = UclidParser.parseModule(input)
+    println("Parse Result: " + module)
   }
 }
