661a1573c761d3cfc343e399962cb9b8e5c22b46
Pramod Subramanyan <pramod@berkeley.edu>
2017-06-12 12:25:06 -0700

    Refactor:  Moved SMT classes to package uclid.smt.

--- src/main/scala/UclidMain.scala
+++ src/main/scala/UclidMain.scala
@@ -1,108 +1,111 @@
-import scala.util.parsing.combinator._
-import scala.collection.immutable._
-import java.nio.file.{Paths, Files}
-import java.nio.charset.StandardCharsets
-import scala.sys.process._
-
-/**
- * Created by Rohit Sinha on 5/23/15.
- */
-object UclidMain {
-  case class UclidOptions(
-      help : Boolean,
-      mainModule: String,
-      srcFiles: List[String]
-  )
+package uclid {
+  import scala.util.parsing.combinator._
+  import scala.collection.immutable._
+  import java.nio.file.{Paths, Files}
+  import java.nio.charset.StandardCharsets
+  import scala.sys.process._
+  import uclid.lang._
   
-  def getOptions(args: Array[String]) : UclidOptions = {
-    def isSwitch(s : String) = (s(0) == '-')
-    var mainModule : String = "main"
-    var srcFiles : List[String] = Nil
-    var help = false
-    var ignore = false;
-   
-    for (i <- args.indices) {
-      if (ignore) {
-        ignore = false
-      } else if ( isSwitch(args(i)) ) {
-        if (args(i) == "--main" || args(i) == "-m") {
-          mainModule = args(i+1)
-          ignore = true
-        } else if (args(i) == "--help" || args(i) == "-h") {
-          help = true;
+  /**
+   * Created by Rohit Sinha on 5/23/15.
+   */
+  object UclidMain {
+    case class UclidOptions(
+        help : Boolean,
+        mainModule: String,
+        srcFiles: List[String]
+    )
+    
+    def getOptions(args: Array[String]) : UclidOptions = {
+      def isSwitch(s : String) = (s(0) == '-')
+      var mainModule : String = "main"
+      var srcFiles : List[String] = Nil
+      var help = false
+      var ignore = false;
+     
+      for (i <- args.indices) {
+        if (ignore) {
+          ignore = false
+        } else if ( isSwitch(args(i)) ) {
+          if (args(i) == "--main" || args(i) == "-m") {
+            mainModule = args(i+1)
+            ignore = true
+          } else if (args(i) == "--help" || args(i) == "-h") {
+            help = true;
+          } else {
+            println("Unknown argument: " + args(i))
+            sys.exit(1)
+          }
         } else {
-          println("Unknown argument: " + args(i))
-          sys.exit(1)
+          srcFiles = args(i) :: srcFiles
         }
-      } else {
-        srcFiles = args(i) :: srcFiles
+      }
+      return UclidOptions(help, mainModule, srcFiles)
+    }
+    
+    type ModuleMap = Map[UclIdentifier, UclModule]
+  
+    val usage = """
+      Usage: UclidMain [options] filename [filenames]
+      Options:
+        -h/--help : This message.
+        -m/--main : Set the main module.
+    """
+    def main(args: Array[String]) {
+      if (args.length == 0) println(usage)
+      val opts = getOptions(args)
+      
+      if (opts.help) {
+        println(usage)
+        sys.exit(0)
+      }
+      val modules = compile(opts.srcFiles)
+      val mainModuleName = UclIdentifier(opts.mainModule)
+      Utils.assert(modules.contains(mainModuleName), "Main module (" + opts.mainModule + ") does not exist.")
+      val mainModule = modules.get(mainModuleName)
+      mainModule match {
+        case Some(m) => execute(m)
+        case None    => 
       }
     }
-    return UclidOptions(help, mainModule, srcFiles)
-  }
-  
-  type ModuleMap = Map[UclIdentifier, UclModule]
-
-  val usage = """
-    Usage: UclidMain [options] filename [filenames]
-    Options:
-      -h/--help : This message.
-      -m/--main : Set the main module.
-  """
-  def main(args: Array[String]) {
-    if (args.length == 0) println(usage)
-    val opts = getOptions(args)
     
-    if (opts.help) {
-      println(usage)
-      sys.exit(0)
-    }
-    val modules = compile(opts.srcFiles)
-    val mainModuleName = UclIdentifier(opts.mainModule)
-    UclidUtils.assert(modules.contains(mainModuleName), "Main module (" + opts.mainModule + ") does not exist.")
-    val mainModule = modules.get(mainModuleName)
-    mainModule match {
-      case Some(m) => execute(m)
-      case None    => 
-    }
-  }
-  
-  def compile(srcFiles : List[String]) : ModuleMap = {
-    type NameCountMap = Map[UclIdentifier, Int]
-    var modules : ModuleMap = Map()
-    var nameCnt : NameCountMap = Map().withDefaultValue(0)
-    
-    for (srcFile <- srcFiles) {
-      println("Input File: " + srcFile)
-      val text = scala.io.Source.fromFile(srcFile).mkString
-      val fileModules = UclidParser.parseModel(text)
-      for(module <- fileModules) {
-        UclidSemanticAnalyzer.checkSemantics(module)
+    def compile(srcFiles : List[String]) : ModuleMap = {
+      type NameCountMap = Map[UclIdentifier, Int]
+      var modules : ModuleMap = Map()
+      var nameCnt : NameCountMap = Map().withDefaultValue(0)
+      
+      for (srcFile <- srcFiles) {
+        println("Input File: " + srcFile)
+        val text = scala.io.Source.fromFile(srcFile).mkString
+        val fileModules = UclidParser.parseModel(text)
+        for(module <- fileModules) {
+          UclidSemanticAnalyzer.checkSemantics(module)
+        }
+        nameCnt = fileModules.foldLeft(nameCnt)((cnts : NameCountMap, m : UclModule) => (cnts + (m.id -> (cnts(m.id) + 1))))
+        val repeatedNameCnt = nameCnt.filter{ case (name, cnt) => cnt > 1 }
+        val repeatedNames = repeatedNameCnt.foldLeft(""){ case (str, (name, cnt)) => str + " " + name }
+        Utils.assert(repeatedNameCnt.size == 0, "Repeated module names: " + repeatedNames)
+        modules = fileModules.foldLeft(modules)((ms: ModuleMap, m : UclModule) => ms + (m.id -> m)) 
       }
-      nameCnt = fileModules.foldLeft(nameCnt)((cnts : NameCountMap, m : UclModule) => (cnts + (m.id -> (cnts(m.id) + 1))))
-      val repeatedNameCnt = nameCnt.filter{ case (name, cnt) => cnt > 1 }
-      val repeatedNames = repeatedNameCnt.foldLeft(""){ case (str, (name, cnt)) => str + " " + name }
-      UclidUtils.assert(repeatedNameCnt.size == 0, "Repeated module names: " + repeatedNames)
-      modules = fileModules.foldLeft(modules)((ms: ModuleMap, m : UclModule) => ms + (m.id -> m)) 
+      println("Total number of modules is: " + modules.size)
+      return modules
     }
-    println("Total number of modules is: " + modules.size)
-    return modules
-  }
-  
-  def execute(module : UclModule) {
+    
+    def execute(module : UclModule) {
       //Control module
       println("Found main module: " + module.id)      
       val asserts = UclidSymbolicSimulator.simulate_steps(module,2)._2 //simulate for 2 steps
       def getCurrentDirectory = new java.io.File( "." ).getCanonicalPath
       asserts.foreach { x => 
         println("*************** Formula Start ***************")
-        var fmla : String = SMTInterface.checkFormulaZ3(x)
+        var fmla : String = uclid.smt.Z3FormulaInterface.checkFormulaZ3(x)
         println(fmla)
         Files.write(Paths.get(getCurrentDirectory + "/tmp.z3"), fmla.getBytes(StandardCharsets.UTF_8))
         //Process("z3 " + getCurrentDirectory + "/tmp.z3 -smt2")
         val z3_output = "z3 " + getCurrentDirectory + "/tmp.z3 -smt2" !!;
         println("Z3 says: " + z3_output)
         println("*************** Formula End ***************")
-        }
+      }
+    }
   }
 }
---
+++ src/main/scala/Z3FormulaInterface.scala
@@ -0,0 +1,138 @@
+
+/**
+ * @author rohitsinha
+ * @author pramod
+ */
+package uclid {
+  package smt {
+    object Z3FormulaInterface {
+      def generateDeclaration(x: Symbol) : String = {
+        def printType(t: Type) : String = {
+          t match {
+            case BoolType() => "Bool"
+            case IntType() => "Int"
+            case MapType(ins,out) =>
+              "(" + ins.foldLeft(""){(acc,i) => 
+                acc + " " + printType(i)} + ") " + printType(out)
+            case ArrayType(ins,out) =>
+              if (ins.size > 1) {
+                "(Array " + ins.foldLeft("(MyTuple" + ins.size){(acc,i) => 
+                  acc + " " + printType(i)} + ") " + printType(out) + ")"
+              } else {
+                "(Array " + printType(ins(0)) + " " + printType(out) + ")"
+              }
+          }
+        }
+        
+        return x.typ match {
+          case BoolType() => "(declare-const " + x.id + " " + printType(x.typ) + ")\n"
+          case IntType() => "(declare-const " + x.id + " " + printType(x.typ) + ")\n"
+          case MapType(ins,out) => 
+            "(declare-fun " + x.id + " " + printType(x.typ) + ")\n"
+          case ArrayType(ins,out) => 
+            "(declare-const " + x.id + " " + printType(x.typ) + ")\n"
+        }
+      }
+      
+      def generateDatatypes(symbols: Set[Symbol]) : String = {
+        var arrayArities : Set[Int] = Set.empty;
+        symbols.foreach { x =>
+          x.typ match {
+            case MapType(ins,out) =>
+              arrayArities = arrayArities ++ Set(ins.size)
+            case ArrayType(ins,out) =>
+              arrayArities = arrayArities ++ Set(ins.size)
+            case _ => ()
+          }
+        }
+    
+        return arrayArities.foldLeft(""){ (acc,x) => 
+          acc + "(declare-datatypes " +
+            "(" + ((1 to x).toList).foldLeft("") {
+              (acc,i) => acc + " " + "T"+i } + ")" +
+            "((MyTuple" + x + " (mk-tuple" + x + 
+            ((1 to x).toList).foldLeft("") { 
+                (acc,i) => acc + " (elem"+i+" T"+i+")" } + 
+            "))))\n"
+        }
+      }
+      
+      def translateExpr(e: Expr) : String = {
+        
+        def mkTuple(index: List[Expr]) : String = {
+          if (index.size > 1) {
+            "(mk-tuple" + index.size + index.foldLeft("")((acc,i) => 
+              acc + " " + translateExpr(i)) + ")" 
+          }
+          else { 
+            translateExpr(index(0)) 
+          }
+        }
+        
+        e match {
+          case Symbol(id,_) => id
+          case OperatorApplication(op,operands) => e.toString
+          case ArraySelectOperation(e, index) =>
+            "(select " + translateExpr(e) + " " + mkTuple(index) + ")"
+          case ArrayStoreOperation(e, index, value) =>
+            "(store " + translateExpr(e) + " " + mkTuple(index) + " " + translateExpr(value) + ")"
+          case FunctionApplication(e, args) =>
+            Utils.assert(e.isInstanceOf[Symbol], "Did beta sub happen?")
+            "(" + translateExpr(e) +
+              args.foldLeft(""){(acc,i) => 
+                acc + " " + translateExpr(i)} + ")"
+          case ITE(e,t,f) =>
+            "(ite " + translateExpr(e) + " " +
+              translateExpr(t) + " " +
+              translateExpr(f) +")"
+          case Lambda(_,_) =>
+            throw new Exception("yo lambdas in assertions should have been beta-reduced")
+          case IntLit(value) => value.toString()
+          case BitVectorLit(_,_) =>
+            throw new Utils.UnimplementedException("Bitvectors unimplemented")
+          case BooleanLit(value) => 
+            value match { case true => "true"; case false => "false" }
+        }
+      }
+      
+      def checkFormulaZ3(e : Expr) : String = {
+        println("Asserting: " + e)
+        val symbols: Set[Symbol] = findSymbolicVariables(e);
+        val decl = symbols.foldLeft(""){(acc,x) => acc + generateDeclaration(x)}
+        val datatypes = generateDatatypes(symbols)
+        val formula = "(assert (not " + translateExpr(e) + "))\n"
+        return datatypes + decl + formula + "(check-sat)\n"
+      }
+      
+      def findSymbolicVariables(es: List[Expr]) : Set[Symbol] = {
+        es.foldLeft(Set.empty[Symbol])((acc,i) => acc ++ findSymbolicVariables(i))
+      }
+      
+      def findSymbolicVariables(e : Expr) : Set[Symbol] = {
+        e match {
+          case Symbol(_,_) =>
+            return Set(e.asInstanceOf[Symbol])
+          case OperatorApplication(op,operands) =>
+            return findSymbolicVariables(operands)
+          case ArraySelectOperation(e, index) =>
+            return findSymbolicVariables(e) ++ findSymbolicVariables(index)
+          case ArrayStoreOperation(e, index, value) =>
+            return findSymbolicVariables(e) ++ 
+              findSymbolicVariables(index) ++ 
+              findSymbolicVariables(value)
+          case FunctionApplication(e, args) =>
+            return findSymbolicVariables(e) ++ findSymbolicVariables(args)
+          case ITE(e,t,f) =>
+            return findSymbolicVariables(e) ++
+              findSymbolicVariables(t) ++
+              findSymbolicVariables(f)
+          case Lambda(_,_) =>
+            throw new Exception("lambdas in assertions should have been beta-reduced")
+          case IntLit(_) => return Set.empty[Symbol]
+          case BitVectorLit(_,_) => return Set.empty[Symbol]
+          case BooleanLit(_) => return Set.empty[Symbol]
+        }
+      }
+    }
+  }
+}
