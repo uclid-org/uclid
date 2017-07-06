8c8f2971636befd4cb7254e6d7f3a13d2e4f1383
Pramod Subramanyan <pramod@berkeley.edu>
2017-07-05 17:53:05 -0700

    forgot to commit this file.

---
+++ src/main/scala/lang/PassManager.scala
@@ -0,0 +1,30 @@
+package uclid
+package lang
+
+import scala.collection.mutable.{ListBuffer,Set} 
+
+class PassManager {
+  type PassList = ListBuffer[ASTAnalysis]
+  var passes : PassList = new PassList()
+  var passNames : Set[String] = Set.empty
+  
+  def addPass(pass : ASTAnalysis) {
+    Utils.assert(!passNames.contains(pass.passName), "Pass with the same name already exists.")
+    passNames += pass.passName
+    passes += pass
+    pass._manager = Some(this)
+  }
+  
+  def run(module : Module) : Option[Module] = {
+    passes.foreach{ _.reset() }
+    
+    val init : Option[Module] = Some(module)
+    return passes.foldLeft(init){
+      (mod, pass) => mod.flatMap((m) => pass.visit(m))
+    }
+  }
+
+  def pass(name : String) : ASTAnalysis = passes.find(_.passName == name).get
+  def doesPassExist(name : String) = passNames.contains(name)
+  def getPass(name : String) : Option[ASTAnalysis] = passes.find((p) => p.passName == name)
+}
