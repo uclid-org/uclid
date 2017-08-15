ff89ff24ecfe7fec1953838193041f0c8ee5f14c
spramod <spramod@79859994-26b0-47d7-b2a6-c933f2ed28e0>
2017-08-15 19:38:39 +0000

    added specifications
    git-svn-id: https://repo.eecs.berkeley.edu/svn/users/spramod/uclid5/trunk@30 79859994-26b0-47d7-b2a6-c933f2ed28e0

--- src/main/scala/UclidSemanticAnalyzer.scala
+++ src/main/scala/UclidSemanticAnalyzer.scala
@@ -8,6 +8,7 @@ class Context {
   var inputs: Map[UclIdentifier, UclType] = _
   var outputs: Map[UclIdentifier, UclType] = _
   var constants: Map[UclIdentifier, UclType] = _
+  var specifications: Map[UclIdentifier, UclExpr] = _
   var types : Map[UclIdentifier, UclType] = _
   var next: List[UclStatement] = _
   var init: List[UclStatement] = _
@@ -51,6 +52,12 @@ class Context {
         "Multiple constants with identical names")
     constants = m_consts.map(x => x.asInstanceOf[T6].id -> x.asInstanceOf[T6].typ).toMap
 
+    type TSpec = UclSpecDecl
+    lazy val m_specs = m.decls.filter { x => x.isInstanceOf[TSpec] }
+    UclidUtils.assert(UclidUtils.allUnique(m_consts.map(i => i.asInstanceOf[TSpec].id)), 
+        "Multiple constants with identical names")
+    specifications = m_specs.map { x => x.asInstanceOf[TSpec].id -> x.asInstanceOf[TSpec].expr } .toMap
+    
     type T7 = UclTypeDecl
     val m_typedecls = m.decls.filter { x => x.isInstanceOf[T7] }
     UclidUtils.assert(UclidUtils.allUnique(m_typedecls.map(i => i.asInstanceOf[T7].id)), 
@@ -73,6 +80,7 @@ class Context {
     copy.inputs = this.inputs
     copy.outputs = this.outputs
     copy.procedures = this.procedures
+    copy.specifications = this.specifications
     copy.types = this.types
     copy.variables = this.variables
     copy.init = this.init
@@ -91,12 +99,12 @@ object UclidSemanticAnalyzer {
   def checkSemantics(m: UclModule) : Unit = {
     var c: Context = new Context()
     c.extractContext(m)
-    check(m,c)
+    checkModule(m,c)
   }
   
-  def check(m: UclModule, c: Context) : Unit = {
+  def checkModule(m: UclModule, c: Context) : Unit = {
     //check module level stuff
-    def checkRedeclarationAndTypes(decls: Map[UclIdentifier, UclType]) : Unit = {
+    def checkRedeclarationAndTypes(decls: Map[UclIdentifier, Any]) : Unit = {
       decls.foreach(i => { UclidUtils.assert(UclidUtils.existsOnce(c.externalDecls(), i._1), 
           "Declaration " + i + " has conflicting name") })
     }
@@ -105,11 +113,12 @@ object UclidSemanticAnalyzer {
     checkRedeclarationAndTypes(c.outputs)
     checkRedeclarationAndTypes(c.variables)
     checkRedeclarationAndTypes(c.types)
+    checkRedeclarationAndTypes(c.specifications)
     
-    m.decls.foreach { x => check(x,c) }
+    m.decls.foreach { x => checkDecl(x,c) }
   }
   
-  def check(d: UclDecl, c: Context) : Unit = {
+  def checkDecl(d: UclDecl, c: Context) : Unit = {
     val externalDecls : List[UclIdentifier] = c.externalDecls()
     d match {
       case UclProcedureDecl(id,sig,decls,body) =>
@@ -119,7 +128,7 @@ object UclidSemanticAnalyzer {
           UclidUtils.assert(UclidUtils.existsOnce(
               (externalDecls ++ sig.inParams.map(j => j._1) ++ sig.outParams.map(j => j._1)), i._1),
               "Signature of procedure " + id + " contains redeclaration: " + i);
-          check(i._2, c)
+          checkType(i._2, c)
         })
      
         decls.foreach { i => {
@@ -131,14 +140,14 @@ object UclidSemanticAnalyzer {
             filter{j => id.value != j.value}.
             foreach{ j => UclidUtils.assert(UclidUtils.existsNone(c.procedures(j).decls.map(k => k.id), i.id), 
                 "Local variable " + i + " redeclared as a local variable of procedure " + j ) }
-          check(i.typ,c)
+          checkType(i.typ,c)
         } }
         
         var c2: Context = c.copyContext()
         c2.inputs = c.inputs ++ (sig.inParams.map(i => i._1 -> i._2).toMap)
         c2.variables = c.variables ++ (sig.outParams.map(i => i._1 -> i._2).toMap)
         c2.variables = c2.variables ++ (decls.map(i => i.id -> i.typ).toMap)
-        body.foreach { x => check(x,c2) }
+        body.foreach { x => checkStmt(x,c2) }
         
       case UclFunctionDecl(id,sig) =>
         UclidUtils.assert(UclidUtils.allUnique(sig.args.map(i => i._1)), 
@@ -147,15 +156,19 @@ object UclidSemanticAnalyzer {
           //check that name is not reused
           UclidUtils.assert(UclidUtils.existsNone(externalDecls, i._1), 
               "Signature of function " + id + " contains redeclaration: " + i)
-          check(i._2, c)
+          checkType(i._2, c)
         })
-      case UclTypeDecl(id,typ) => check(typ, c)
-      case UclStateVarDecl(id, typ) => check(typ, c)
-      case UclInputVarDecl(id,typ) => check(typ, c)
-      case UclOutputVarDecl(id, typ) => check(typ, c)
-      case UclConstantDecl(id, typ) => check(typ, c)
-      case UclInitDecl(body) => body.foreach{x => check(x,c)}
-      case UclNextDecl(body) => body.foreach{x => check(x,c)}
+      case UclTypeDecl(id,typ)       => checkType(typ, c)
+      case UclStateVarDecl(id, typ)  => checkType(typ, c)
+      case UclInputVarDecl(id,typ)   => checkType(typ, c)
+      case UclOutputVarDecl(id, typ) => checkType(typ, c)
+      case UclConstantDecl(id, typ)  => checkType(typ, c)
+      case UclSpecDecl(id, expr)     => 
+        checkExpr(expr, c)
+        UclidUtils.assert(transitiveType(typeOf(expr, c)._1, c) == UclBoolType, 
+            "Expressions in specification declarations must have Boolean type.")
+      case UclInitDecl(body)         => body.foreach{x => checkStmt(x,c)}
+      case UclNextDecl(body)         => body.foreach{x => checkStmt(x,c)}
     }
   }
   
@@ -182,7 +195,7 @@ object UclidSemanticAnalyzer {
     } else { return typ }
   }
   
-  def check(typ: UclType, c: Context) : Unit = {
+  def checkType(typ: UclType, c: Context) : Unit = {
     val externalDecls : List[UclIdentifier] = c.externalDecls()
     if (typ.isInstanceOf[UclEnumType]) {
       typ.asInstanceOf[UclEnumType].ids.foreach { i => 
@@ -195,33 +208,33 @@ object UclidSemanticAnalyzer {
         UclidUtils.assert(!(transitiveType(i._2,c).isInstanceOf[UclMapType] || 
             transitiveType(i._2,c).isInstanceOf[UclArrayType]), 
             "Records cannot contain maps or arrays")
-        check(i._2, c)
+        checkType(i._2, c)
       }
     } else if (typ.isInstanceOf[UclMapType]) {
       typ.asInstanceOf[UclMapType].inTypes.foreach { i => 
         UclidUtils.assert(!(transitiveType(i,c).isInstanceOf[UclMapType]), 
             "Map types cannot be indexed by maps: " + typ);
-        check(i,c) 
+        checkType(i,c) 
         }
       UclidUtils.assert(!(transitiveType(typ.asInstanceOf[UclMapType].outType,c).isInstanceOf[UclMapType]), 
           "Map types cannot produce maps: " + typ)
-      check(typ.asInstanceOf[UclMapType].outType, c)
+      checkType(typ.asInstanceOf[UclMapType].outType, c)
     } else if (typ.isInstanceOf[UclArrayType]) {
       typ.asInstanceOf[UclArrayType].inTypes.foreach { i => 
         UclidUtils.assert(!(transitiveType(i,c).isInstanceOf[UclArrayType]), 
             "Array types cannot be indexed by arrays: " + typ);
-        check(i,c) 
+        checkType(i,c) 
         }
       UclidUtils.assert(!(transitiveType(typ.asInstanceOf[UclArrayType].outType,c).isInstanceOf[UclArrayType]), 
           "Array types cannot produce arrays: " + typ)
-      check(typ.asInstanceOf[UclArrayType].outType, c)
+      checkType(typ.asInstanceOf[UclArrayType].outType, c)
     } else if (typ.isInstanceOf[UclSynonymType]) {
       UclidUtils.assert(c.types.keys.exists { x => x.value == typ.asInstanceOf[UclSynonymType].id.value }, 
           "Synonym Type " + typ + " does not exist.")
     }
   }
 
-  def typeOf(lhs: UclLhs, c: Context) : UclType = {
+  def typeOfLHS(lhs: UclLhs, c: Context) : UclType = {
     var intermediateType : UclType = (c.outputs ++ c.variables)(lhs.id)
     lhs.arraySelect match {
       case Some(as) => 
@@ -244,7 +257,7 @@ object UclidSemanticAnalyzer {
     }
   }
   
-  def check(lhs: UclLhs, c: Context) : Unit = {
+  def checkLHS(lhs: UclLhs, c: Context) : Unit = {
     UclidUtils.assert((c.outputs.keys ++ c.variables.keys).exists { x => x.value == lhs.id.value }, 
         "LHS variable " + lhs.id + " does not exist")
     var intermediateType = transitiveType((c.outputs ++ c.variables)(lhs.id),c)
@@ -254,7 +267,7 @@ object UclidSemanticAnalyzer {
         UclidUtils.assert(intermediateType.isInstanceOf[UclArrayType],
             "Cannot use select on non-array " + lhs.id)
         intermediateType = transitiveType(intermediateType.asInstanceOf[UclArrayType].outType,c)
-        index.foreach { x => check(x,c) }
+        index.foreach { x => checkExpr(x,c) }
       case None => ()
     }
     lhs.recordSelect match {
@@ -271,46 +284,55 @@ object UclidSemanticAnalyzer {
     }
   }
   
-  def check(s: UclStatement, c: Context) : Unit = {
+  def assertNoTemporalType(t: (UclType, Boolean), e: UclExpr) : Unit = 
+    UclidUtils.assert(!t._2, "No temporal operators allowed in expression " + e)
+  
+  def checkStmt(s: UclStatement, c: Context) : Unit = {
     val externalDecls : List[UclIdentifier] = c.externalDecls()
     s match {
-      case UclAssertStmt(e) => check(e,c)
-      case UclAssumeStmt(e) => check(e,c)
+      case UclAssertStmt(e) => checkExpr(e,c)
+      case UclAssumeStmt(e) => checkExpr(e,c)
       case UclHavocStmt(id) => 
         UclidUtils.assert((c.variables.keys ++ c.outputs.keys).exists { x => x.value == id.value }, 
             "Statement " + s + " updates unknown variable")
       case UclAssignStmt(lhss, rhss) => 
         UclidUtils.assert(lhss.size == rhss.size, "LHSS and RHSS of different size: " + s);
-        lhss.foreach{ x => check(x,c) }; rhss.foreach { x => check(x,c) };
-        UclidUtils.assert((lhss zip rhss).forall { i => typeOf(i._1, c) == typeOf(i._2, c)._1 && !typeOf(i._2, c)._2}, 
+        lhss.foreach{ x => checkLHS(x,c) }; rhss.foreach { x => checkExpr(x,c) };
+        UclidUtils.assert((lhss zip rhss).forall 
+            { i => 
+              val rhsType = typeOf(i._2, c)
+              typeOfLHS(i._1, c) == rhsType._1 && !rhsType._2}, 
             "LHSS and RHSS have conflicting types: " + s);
         UclidUtils.assert(lhss.distinct.size == lhss.size, "LHSS contains identical variables: " + s)
       case UclIfElseStmt(e, t, f) => 
-        check(e,c); 
-        UclidUtils.assert(transitiveType(typeOf(e,c)._1,c) == UclBoolType() && !typeOf(e,c)._2, 
-            "Expected boolean conditional");
-        (t ++ f).foreach { x => check(x,c) };
+        checkExpr(e,c); 
+        val eType = typeOf(e,c)
+        assertNoTemporalType(eType,e)
+        UclidUtils.assert(transitiveType(eType._1,c) == UclBoolType(), 
+            "Conditionals in if statements must have boolean type.");
+        (t ++ f).foreach { x => checkStmt(x,c) };
       case UclForStmt(id,_,body) => 
         UclidUtils.assert(!(externalDecls.exists { x => x.value == id.value }), 
             "For Loop counter " + id + " redeclared");
         var c2: Context = c.copyContext();
         c2.inputs = c.inputs ++ Map(id -> UclIntType());
-        body.foreach{x => check(x,c2)}
+        body.foreach{x => checkStmt(x,c2)}
       case UclCaseStmt(body) => body.foreach { x =>
-        check(x._1,c);
-        UclidUtils.assert(transitiveType(typeOf(x._1,c)._1,c) == UclBoolType() && !typeOf(x._1,c)._2, 
+        checkExpr(x._1,c);
+        val xType = typeOf(x._1,c)
+        UclidUtils.assert(transitiveType(xType._1,c) == UclBoolType() && !xType._2, 
             "Expected boolean conditional within case statement guard");
-        x._2.foreach { y => check(y,c) } 
+        x._2.foreach { y => checkStmt(y,c) } 
         }
       case UclProcedureCallStmt(id, lhss, args) =>
         UclidUtils.assert(UclidUtils.existsOnce(c.procedures.keys.toList, id), 
             "Calling unknown procedure " + id)
-        lhss.foreach{ x => check(x,c) }
-        args.foreach{ x => check(x,c) }
+        lhss.foreach{ x => checkLHS(x,c) }
+        args.foreach{ x => checkExpr(x,c) }
         UclidUtils.assert(lhss.size == c.procedures(id).sig.outParams.size, 
             "Calling procedure " + id + " with incorrect number of results")
         UclidUtils.assert((lhss zip c.procedures(id).sig.outParams.map(i => i._2)).
-            forall { i => typeOf(i._1, c) == i._2 }, 
+            forall { i => typeOfLHS(i._1, c) == i._2 }, 
             "Calling procedure " + id + " with results of incorrect type");
         UclidUtils.assert(args.size == c.procedures(id).sig.inParams.size, 
             "Calling procedure " + id + " with incorrect number of arguments")
@@ -337,22 +359,24 @@ object UclidSemanticAnalyzer {
     }
     
     e match {
-      case UclBiImplication(l,r) =>
-        return typeOfBinaryBooleanOperator(l,r)
-      case UclImplication(l,r) =>
-        return typeOfBinaryBooleanOperator(l,r)
-      case UclConjunction(l,r) => 
-        return typeOfBinaryBooleanOperator(l,r)
-      case UclDisjunction(l,r) => 
-        return typeOfBinaryBooleanOperator(l,r)
+      case UclBiImplication(l,r) => return typeOfBinaryBooleanOperator(l,r)
+      case UclImplication(l,r)   => return typeOfBinaryBooleanOperator(l,r)
+      case UclConjunction(l,r)   => return typeOfBinaryBooleanOperator(l,r)
+      case UclDisjunction(l,r)   => return typeOfBinaryBooleanOperator(l,r)
       case UclNegation(e) => 
         val (typeE, tempE) = typeOf(e,c)
         assertBoolType(typeE);
         return (UclBoolType(), tempE)
       case UclEquality(l,r) => 
-        UclidUtils.assert(typeOf(l,c)._1 == typeOf(r,c)._1 && transitiveType(typeOf(l,c)._1,c) == UclIntType(),
-            "Equality operator requires Int arguments.")
-        return (UclBoolType(), typeOf(l,c)._2 || typeOf(r,c)._2) //TODO: consult Markus
+        val (typeL, tempL) = typeOf(l,c)
+        UclidUtils.assert(transitiveType(typeL,c) == UclIntType(), 
+            "Equality operator requires Int arguments. Lefthand argument side is of type " + typeL + "instead")
+        val (typeR, tempR) = typeOf(r,c)
+        UclidUtils.assert(transitiveType(typeR,c) == UclIntType(), 
+            "Equality operator requires Int arguments. Righthand argument side is of type " + typeR + "instead")
+//        UclidUtils.assert(typeL == typeR,
+//            "Equality operator requires equally typed arguments.")
+        return (UclBoolType(), tempL || tempR) //TODO: consult Markus
       case UclIFuncApplication(op,es) =>
         lazy val types = es.map { e => typeOf (e,c) }
         /**
@@ -422,32 +446,40 @@ object UclidSemanticAnalyzer {
     }    
   }
   
-  def check(e: UclExpr, c: Context) : Unit = {
+  def checkExpr(e: UclExpr, c: Context) : Unit = {
     val externalDecls : List[UclIdentifier] = c.externalDecls()
      e match { //check that all identifiers in e have been declared
-       case UclBiImplication(l,r) => check(r,c); check(l,c);
-       case UclImplication(l,r) => check(r,c); check(l,c);
-       case UclConjunction(l,r) => check(r,c); check(l,c);
-       case UclDisjunction(l,r) => check(r,c); check(l,c);
-       case UclNegation(l) => check(l,c);
-       case UclEquality(l,r) => check(r,c); check(l,c);
-       case UclIFuncApplication(op,args) => args.foreach { x => check(x,c) }
-       case UclArraySelectOperation(a,index) => check(a,c); index.foreach { x => check(x,c) }
+       case UclBiImplication(l,r) => checkExpr(r,c); checkExpr(l,c);
+       case UclImplication(l,r) => checkExpr(r,c); checkExpr(l,c);
+       case UclConjunction(l,r) => checkExpr(r,c); checkExpr(l,c);
+       case UclDisjunction(l,r) => checkExpr(r,c); checkExpr(l,c);
+       case UclNegation(l) => checkExpr(l,c);
+       case UclEquality(l,r) => checkExpr(r,c); checkExpr(l,c);
+       case UclIFuncApplication(op,args) => args.foreach { x => checkExpr(x,c) }
+       case UclArraySelectOperation(a,index) => checkExpr(a,c); index.foreach { x => checkExpr(x,c) }
        case UclArrayStoreOperation(a,index,value) => 
-         check(a,c); index.foreach { x => check(x,c) }; check(value, c);
-       case UclFuncApplication(f,args) => check(f,c); args.foreach { x => check(x,c) }
-       case UclITE(cond,t,f) => check(cond,c); check(t,c); check(f,c);
+         checkExpr(a,c); index.foreach { x => checkExpr(x,c) }; checkExpr(value, c);
+       case UclFuncApplication(f,args) => checkExpr(f,c); args.foreach { x => checkExpr(x,c) }
+       case UclITE(cond,t,f) => 
+         checkExpr(cond,c) 
+         val tTypeCond = transitiveType(typeOf(cond, c)._1, c)
+         UclidUtils.assert(tTypeCond.isInstanceOf[UclBoolType], 
+             "Conditional must have boolean type in expression " + e + " but has type " + tTypeCond)
+         checkExpr(t,c) 
+         checkExpr(f,c)
+         UclidUtils.assert(transitiveType(typeOf(t, c)._1, c) == transitiveType(typeOf(f, c)._1, c), 
+             "The branches in the ITE expression " + e + " have different types.")
        case UclLambda(ids,le) => ids.foreach { 
-           x => check(x._2,c);
+           x => checkType(x._2,c);
            UclidUtils.assert(transitiveType(x._2,c).isInstanceOf[UclIntType] || 
                transitiveType(x._2,c).isInstanceOf[UclBoolType],
-             "Lambda indexed by non-primitive type");
+             "Lambda indexed by non-primitive type in expression " + e);
          }
          ids.foreach{ x => UclidUtils.assert((externalDecls ++ ids.map(i => i._1)).
              count { i => i.value == x._1.value } == 1, "Lambda argument has a redeclaration") }
          var c2: Context = c.copyContext()
          c2.inputs = c.inputs ++ (ids.map(i => i._1 -> i._2).toMap)
-         check(le,c2);
+         checkExpr(le,c2);
        case UclIdentifier(id) => 
          UclidUtils.assert((c.constants.keys ++ c.inputs.keys ++ c.outputs.keys ++ c.variables.keys).
          exists{i => i.value == id}, "Identifier " + id + " not found");
