cc05da3ac8ff51f894b81bac146e7b8ef696309a
Pramod <psubrama@princeton.edu>
2017-09-21 11:33:03 -0700

    Record assignment rewriter pass.

---
+++ src/main/scala/lang/RecordAssignmentRewriter.scala
@@ -0,0 +1,17 @@
+package uclid
+package lang
+
+abstract class RecordAssignmentRewriterPass(typechecker: TypecheckPass) extends RewritePass {
+  def expandAssignment(lhs: Lhs, rhs: Expr) : List[(Lhs, Expr)]
+  
+  override def rewriteAssign(st : AssignStmt, ctx : ScopeMap) : List[Statement] = {
+    val pairs = (st.lhss zip st.rhss).flatMap((p) => expandAssignment(p._1, p._2))
+    val assign = AssignStmt(pairs.map(_._1), pairs.map(_._2))
+    return List(assign)
+  }
+}
+
+/*
+class RecordAssignmentRewriter extends ASTRewriter(
+    "RecordAssignmentRewriter", new RecordAssignmentRewriterPass())
+*/
