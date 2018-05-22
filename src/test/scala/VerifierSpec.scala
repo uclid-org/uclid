2311622868f68584ccc33eb166b3af8f997be51a
Pramod Subramanyan <pramod.subramanyan@gmail.com>
2018-05-22 18:02:15 +0530

    Add a new test that does array update.

--- src/test/scala/VerifierSpec.scala
+++ src/test/scala/VerifierSpec.scala
@@ -71,6 +71,9 @@ class VerifierSanitySpec extends FlatSpec {
   "test-array-2.ucl" should "verify successfully." in {
     VerifierSpec.expectedFails("./test/test-array-2.ucl", 1)
   }
+  "array-update.ucl" should "verify successfully." in {
+    VerifierSpec.expectedFails("./test/array-update.ucl", 0)
+  }
   "test-array-1-unsafe.ucl" should "verify all but 4 assertions." in {
     VerifierSpec.expectedFails("./test/test-array-1-unsafe.ucl", 4)
   }
