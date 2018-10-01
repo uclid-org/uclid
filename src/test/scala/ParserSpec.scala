b60ed85bde5b35ad784ab28787c62f9fedb28d32
Deepak Sirone <deepaksirone94@gmail.com>
2018-10-01 12:19:01 +0530

    Add tests to ParserSpec

--- src/test/scala/ParserSpec.scala
+++ src/test/scala/ParserSpec.scala
@@ -464,7 +464,26 @@ class ParserSpec extends FlatSpec {
       assert (false)
     } catch {
       case p : Utils.ParserErrorList =>
-        assert (p.errors(0)._1.contains("HyperSelect can only be used in a module-level expression"))
+        assert (p.errors(0)._1.contains("Trace select can only be used in a module-level expression"))
+    }
+  }
+
+  "test-hyperproperty-2.ucl" should "not parse successfully." in {
+    try {
+      val fileModules = UclidMain.compile(List(new File("test/test-hyperproperty-2.ucl")), lang.Identifier("main"))
+      assert (false)
+    } catch {
+      case p : Utils.ParserErrorList =>
+        assert (p.errors(0)._1.contains("Trace select can only be used in a verification expression"))
+    }
+  }
+  "test-hyperproperty-3.ucl" should "not parse successfully." in {
+    try {
+      val fileModules = UclidMain.compile(List(new File("test/test-hyperproperty-3.ucl")), lang.Identifier("main"))
+      assert (false)
+    } catch {
+      case p : Utils.ParserErrorList =>
+        assert (p.errors(0)._1.contains("Trace select can only be used in a verification expression"))
     }
   }
 }
