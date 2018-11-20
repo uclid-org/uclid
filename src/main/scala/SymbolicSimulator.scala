260eece74656e23e39fd5ca7c2b268bebe012f9f
Deepak Sirone <deepaksirone94@gmail.com>
2018-11-21 00:44:13 +0530

    Fix CEXs

--- src/main/scala/SymbolicSimulator.scala
+++ src/main/scala/SymbolicSimulator.scala
@@ -162,7 +162,7 @@ class SymbolicSimulator (module : Module) {
             //symbolicSimulateLambdas(0, cmd.args(0)._1.asInstanceOf[IntLit].value.toInt, true, false, context, label, noLTLFilter, solver)
             //initialize(false, true, false, context, label, noLTLFilter)
             //symbolicSimulate(0, cmd.args(0)._1.asInstanceOf[IntLit].value.toInt, true, false, context, label, noLTLFilter)
-            runLazySC(cmd.args(0)._1.asInstanceOf[IntLit].value.toInt, context, "lazy_SC", noLTLFilter, solver)
+            runLazySC(cmd.args(0)._1.asInstanceOf[IntLit].value.toInt, context, label, noLTLFilter, solver)
           case "bmc" =>
             val label : String = cmd.resultVar match {
               case Some(l) => l.toString()
