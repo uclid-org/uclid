33d0d4b94149b26034edacbb343a5a8248d7b156
Pramod Subramanyan <pramod@berkeley.edu>
2019-11-04 16:15:50 +0530

    first baby proof is working.

--- src/main/scala/uclid/lang/modelcounts/UMCParser.scala
+++ src/main/scala/uclid/lang/modelcounts/UMCParser.scala
@@ -1,28 +1,50 @@
 package uclid.lang.modelcounts
 
 import uclid.{lang => l}
+import uclid.lang.Identifier
+import uclid.smt.IntLit
 
 
 class UMCParser extends l.UclidParser {
   lazy val KwProof = "proof"
   lexical.reserved += (KwProof)
 
+  lazy val ControlBlock : List[l.GenericProofCommand] = List(
+    l.GenericProofCommand(
+        l.Identifier("unroll"), 
+        List.empty, List((l.IntLit(1), "1")), 
+        Some(l.Identifier("v")), None),
+    l.GenericProofCommand(
+        l.Identifier("check"), 
+        List.empty, List.empty, 
+        None, None),
+    l.GenericProofCommand(
+        l.Identifier("print_results"), 
+        List.empty, List.empty, 
+        None, None),
+    l.GenericProofCommand(
+        l.Identifier("print_cex"), 
+        List.empty, List.empty, 
+        None, Some(l.Identifier("v")))
+  )
+
   lazy val UMCDecl: PackratParser[uclid.lang.Decl] =
     positioned (TypeDecl | DefineDecl)
 
-  lazy val AssertStmt: PackratParser[uclid.lang.Statement] = positioned {
-    KwAssert ~> Expr <~ ";" ^^ { case e => l.AssertStmt(e, None) }
+  lazy val AssertDecl: PackratParser[uclid.lang.SpecDecl] = positioned {
+    KwAssert ~> Id ~ (":" ~> Expr <~ ";") ^^ {
+      case id ~ e => l.SpecDecl(id, e, List.empty)
+    }
   }
-  lazy val ProofStmt: PackratParser[l.Statement] = 
-    positioned ( AssertStmt );
-  lazy val ProofScript: PackratParser[List[l.Statement]] = {
+  lazy val ProofStmt: PackratParser[l.Decl] = 
+    positioned ( AssertDecl );
+  lazy val ProofScript: PackratParser[List[l.Decl]] = {
     KwProof ~ "{" ~> rep(ProofStmt) <~ "}"
   }
   lazy val UMCModule: PackratParser[l.Module] = positioned {
     KwModule ~> Id ~ ("{" ~> rep(UMCDecl)) ~ (ProofScript <~ "}") ^^ {
       case id ~ decls ~ proof => {
-        val next = l.NextDecl(l.BlockStmt(List.empty, proof))
-        uclid.lang.Module(id, next :: decls, List.empty, List.empty)
+        uclid.lang.Module(id, decls ++ proof, ControlBlock, List.empty)
       }
     }
   }
