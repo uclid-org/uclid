c71feab0aef8548f67bab692a5c48b9d2808465c
Pranav Gaddamadugu <pranavsaig@gmail.com>
2020-03-16 15:55:38 -0700

    Changed authors

--- src/test/scala/smt/SExprParserSpec.scala
+++ src/test/scala/smt/SExprParserSpec.scala
@@ -31,7 +31,7 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * Authors: Norman Mu, Pramod Subramanyan
+ * Authors: Pranav Gaddamadugu
  *
  * UCLID Parser/Typechecker tests.
  *
