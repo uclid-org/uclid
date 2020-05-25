1a04612a15747d1fa25f36c95fdf9616b42fb2c2
Kevin Läufer <laeufer@cs.berkeley.edu>
2020-05-25 10:02:56 -0700

    z3: make it easier to change versions by moving to a generic folder

--- get-z3-linux.sh
+++ get-z3-linux.sh
@@ -1,17 +1,23 @@
 #! /bin/bash
 
+VERSION=4.8.6
+
 # HOW TO USE THIS SCRIPT?
 #
 # Run the following command: source get-z3-linux.sh from your bash prompt.
 #
-# This will download z3-4.8.6 and install z3 4.8.6.
+# This will download z3-$VERSION and install z3 $VERSION.
 #
 # Subsequently, source the setup-z3-linux.sh script to bring Z3 into your
 # path.  This latter step will have to be done repeatedly. (Or you can
 # include it in your bashrc.)
 
-wget https://github.com/Z3Prover/z3/releases/download/z3-4.8.6/z3-4.8.6-x64-ubuntu-16.04.zip
-unzip z3-4.8.6-x64-ubuntu-16.04.zip
-cp z3-4.8.6-x64-ubuntu-16.04/bin/com.microsoft.z3.jar lib/
+wget https://github.com/Z3Prover/z3/releases/download/z3-$VERSION/z3-$VERSION-x64-ubuntu-16.04.zip
+unzip z3-$VERSION-x64-ubuntu-16.04.zip
 
+# rename folder to a generic name to make it easier to upgrade z3 versions
+rm -rf z3/
+mv z3-$VERSION-x64-ubuntu-16.04/ z3/
+
+cp z3/bin/com.microsoft.z3.jar lib/
 source setup-z3-linux.sh
