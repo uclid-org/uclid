8c9832ca67ccba5ebf3bd90cec7e2dbed0ec431b
Shubham Sahai <ssahai@cse.iitk.ac.in>
2020-09-30 15:01:54 +0530

    update author info in modelcounting extension

--- src/main/scala/uclid/extensions/modelcounts/UMCRewriter.scala
+++ src/main/scala/uclid/extensions/modelcounts/UMCRewriter.scala
@@ -31,7 +31,7 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * Author: Pramod Subramanyan
+ * Author: Pramod Subramanyan, Shubham Sahai
  *
  * Rewriter for the UCLID5 model counter.
  *
