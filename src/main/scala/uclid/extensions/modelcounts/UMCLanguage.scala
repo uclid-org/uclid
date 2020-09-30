103a771614b0660d20ed633623ef08a602d3d82b
Shubham Sahai <ssahai@cse.iitk.ac.in>
2020-09-30 15:01:54 +0530

    update author info in modelcounting extension

--- src/main/scala/uclid/extensions/modelcounts/UMCLanguage.scala
+++ src/main/scala/uclid/extensions/modelcounts/UMCLanguage.scala
@@ -30,7 +30,7 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * Author: Pramod Subramanyan
+ * Author: Pramod Subramanyan, Shubham Sahai
  *
  * Main file for the UCLID model counter.
  *
