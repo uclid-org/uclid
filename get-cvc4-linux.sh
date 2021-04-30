e6a90190b63e8f94018b80e4a552492ea9b41711
Elizabeth Polgreen <epolgreen@gmail.com>
2021-04-30 12:48:33 +0100

    debugging

--- get-cvc4-linux.sh
+++ get-cvc4-linux.sh
@@ -9,7 +9,6 @@
 wget https://github.com/CVC4/CVC4/releases/download/1.8/cvc4-1.8-x86_64-linux-opt
 mv cvc4-1.8-x86_64-linux-opt cvc4
 chmod 755 cvc4
-
-# rename folder to a generic name to make it easier to upgrade z3 versions
 export PATH=$PATH:$PWD
+which cvc4
 
