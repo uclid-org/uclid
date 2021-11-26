d3a51aa1c1fb0f9826099e9f9a1c6eebb1ef9309
Kevin Cheang <kcheang@berkeley.edu>
2021-11-26 10:28:15 -0800

    Updated Makefile to reflect paper runtimes and
    Added instructions for running the Boogie proofs.

--- tool_paper_examples/run_TrustedAbstractPlatform.sh
+++ tool_paper_examples/run_TrustedAbstractPlatform.sh
@@ -3,10 +3,23 @@
 echo "Moving into TrustedAbstractPlatform directory"
 cd TrustedAbstractPlatform
 
-echo "Generating TAP models ..."
+echo "Verifying TAP model ..."
 cd modules
 time make tap
 
-echo "Running Proofs ..."
+echo "Verify integrity proof ..."
 cd ../proofs
 time make
+
+echo "Returning to examples directory ..."
+cd ../../
+
+echo "Cloning Boogie TAP models repo ..."
+git clone git@github.com:0tcb/TAP.git
+
+echo "Verifying TAP models in Boogie ..."
+cd TAP/AbstractPlatform
+time boogie CPU.bpl CPUImpl.bpl Types.bpl ../Common/Cache.bpl ../Common/CacheImpl.bpl ../Common/Common.bpl ../Common/Types.bpl
+
+echo "Verifying integrity proof model in Boogie ..."
+time boogie IntegrityProof.bpl CPU.bpl CPUImpl.bpl Types.bpl ../Common/Cache.bpl ../Common/CacheImpl.bpl ../Common/Common.bpl ../Common/Types.bpl ProofCommon.bpl /proc:ProveIntegrity
