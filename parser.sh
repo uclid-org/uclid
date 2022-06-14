44b8cd98e91f9ee2e7b3348bc556bfc70ba8f695
lichye <yeleiqi@hotmail.com>
2022-06-14 16:24:13 -0400

    write a auto error-creater

---
+++ parser.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+python3 parser.py
+dir="./parser_test/*"
+sbt universal:packageBin
+cd target/universal
+rm -rf uclid-0.9.5
+unzip uclid-0.9.5.zip
+cd uclid-0.9.5
+export PATH=$PATH:$PWD/bin
+cd ..
+cd ..
+cd ..
+for file in $dir
+do
+  filename=$(basename "$file" .ucl)
+  echo uclid "parser_test/"$filename.ucl
+  uclid "parser_test/"$filename.ucl
+done
