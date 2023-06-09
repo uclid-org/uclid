b1f3105c04a9e98d563f4d92d4ea96e2880ada9c
E Polgreen <epolgreen@gmail.com>
2023-06-09 23:32:16 +0100

    Revert "make scripts for mac support m1"
    This reverts commit bfef21906e923ba7a9fb8b97290e858644098711.

--- get-cvc5-macos.sh
+++ get-cvc5-macos.sh
@@ -6,19 +6,11 @@
 # This will download cvc5 version 0.0.4 and add it to your path
 #
 
-mac_arch=$(uname -m)
-
-if [ $mac_arch="arm64" ]; then
-    FILE_TO_GET="cvc5-macOS-arm64"
-else
-    FILE_TO_GET="cvc5-macOS"
-fi
-
-wget https://github.com/cvc5/cvc5/releases/download/cvc5-1.0.3/$FILE_TO_GET
+wget https://github.com/cvc5/cvc5/releases/download/cvc5-1.0.3/cvc5-macOS
 mkdir -p cvc5/bin/
-mv $FILE_TO_GET cvc5/bin/cvc5
+mv cvc5-macOS cvc5/bin/cvc5
 chmod 755 cvc5/bin/cvc5
-cp cvc5_wait.sh cvc5/bin/cvc5_wait.sh
+mv cvc5_wait.sh cvc5/bin/cvc5_wait.sh
 export PATH=$PATH:$PWD/cvc5/bin
 cvc5 --version
 
