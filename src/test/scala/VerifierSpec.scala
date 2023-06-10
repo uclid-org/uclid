ba7063dbab273eb8a0829ee6303ff82b4798c57b
lichye <yeleiqi@hotmail.com>
2023-06-10 12:02:43 +0800

    fixup assert at base case
    add corresponding tests
    
    fix little bugs
    
    fix up little bugs
    
    fix little bugs

--- src/test/scala/VerifierSpec.scala
+++ src/test/scala/VerifierSpec.scala
@@ -133,6 +133,12 @@ class VerifierSanitySpec extends AnyFlatSpec {
   "test-assert-3.ucl" should "verify all assertions." in {
     VerifierSpec.expectedFails("./test/test-assert-3.ucl", 0)
   }
+  "test-assert-4.ucl" should "fail to verify five assertions." in {
+    SMTLIB2Spec.expectedFails("./test/test-assert-4.ucl", 2)
+  }
+  "test-assert-5.ucl" should "fail to verify five assertions." in {
+    SMTLIB2Spec.expectedFails("./test/test-assert-5.ucl", 1)
+  }
   "test-primed-variables-1.ucl" should "verify all assertions." in {
     VerifierSpec.expectedFails("./test/test-primed-variables-1.ucl", 0)
   }
