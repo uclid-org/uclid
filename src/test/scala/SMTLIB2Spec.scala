97540fe4140c5e2bc2094c744b2cc67f4ecc5b7d
FedericoAureliano <fmorarocha@gmail.com>
2024-07-03 21:59:57 -0700

    add test case to CI

--- src/test/scala/SMTLIB2Spec.scala
+++ src/test/scala/SMTLIB2Spec.scala
@@ -445,6 +445,9 @@ class SMTLIB2Spec extends AnyFlatSpec {
   "issue-187b.ucl" should "verify all assertions." in {
     SMTLIB2Spec.expectedFails("./test/issue-187b.ucl", 0)
   }
+  "issue-253.ucl" should "verify all assertions." in {
+    SMTLIB2Spec.expectedFails("./test/issue-253.ucl", 0)
+  }
   "test-redundant-assign.ucl" should "verify all assertions." in {
     SMTLIB2Spec.expectedFails("./test/test-redundant-assign.ucl", 0)
   }
